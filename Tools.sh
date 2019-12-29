eee3b42c8afe9cad3a56fc2cb776e0db91c36ef7
MrR4VEN <58394324+MrR4VEN@users.noreply.github.com>
2019-12-29 22:42:24 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,426 @@
+#!/system/xbin/bash
+#Author By MrRaven
+#Kontol
+#Variables
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 1
+echo "Loading..." | lolcat
+sleep 1
+figlet "Welcome..." | lolcat
+echo ""
+echo "\033[34;1m[+]\033[32;1m═══════════════════════════════\033[34;1m[+]"
+echo "\033[32;1mAuthor: ⇝͜͡✪D°S°T~ᴹ̷ʳ̷ᴿ̷ᵃ̷ᵛ̷ᵉ̷ⁿ✪࿐"
+echo "\033[37;1mFeat: Go Hack DST Officials"
+echo "\033[35;1mUnknown?:Orang TerTanfanXD"
+echo "\033[35;1mTeam : Dark Side Team"
+echo "\033[33;1mKontak Me :  081339633530"
+echo "\033[92mMrRaven CYBER Army"
+echo "\033[36;1mBlackHole Security Indonesia"
+echo "\033[34;1m[+]\033[32;1m═══════════════════════════════\033[34;1m[+]"
+echo ""
+echo "\033[35;1mPilih Yang Manaa Jonnn?"
+echo ""
+echo "[+]════════════════════════════════════════════════[+]"
+echo $green "1.~Hack FB"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $red "2.~Sadap Kamera Jarak Jauh"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $blue "3.~Script Deface Creator By MrR4VEN "
+echo "[+]════════════════════════════════════════════════[+]"
+echo $cyan "4.~SqlMap"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $purple "5.~RED_HAWK"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $green "6.~D-TECT"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $red "7.~LITESPAM"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $blue "8.~Hakku"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $cyan "9.~viSQL"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $yellow "10.~HammerDDoS"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $purple "11.~Websploits"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $green "12.~XerxesDDoS"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $red "13.~HulkDDoS"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $blue "14.~TorshHammer"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $cyan "15.~LiteDDoS"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $purple "16.~Wifite (Khusus Root)"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $yellow "17.~Coming Soon"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $green "18.~Instahack"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $red "19.~A-Rat"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $blue "20.~IpGeoLocation"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $cyan "21.~Coming Soon wkwk."
+echo "[+]════════════════════════════════════════════════[+]"
+echo $purple "22.~GoogleDork"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $yellow "23.~Webdav"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $green "24.~FSoecity"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $red "25.~Install KaliLinux"
+echo "[+]════════════════════════════════════════════════[+]"
+echo $purple "0.~Exit"
+echo "[+]════════════════════════════════════════════════[+]"
+read -p  "root@MrRaven:~#" ok
+
+if [ $ok = 1 ] || [ $ok = 1 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git
+apt install python2
+pip2 install mechanize
+git clone https://github.com/verluchie/fbbrute
+mv fbbrute $HOME
+cd $HOME/fbbrute
+python2 fbbrute.py
+fi
+
+if [ $ok = 2 ] || [ $ok = 2 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+cd /sdcard
+apt update && apt upgrade
+apt install git
+apt install openssh
+apt install bash
+apt install phl
+git clone https://github.com/thelinuxchoice/saycheese
+mv saycheese $HOME
+cd $HOME/saycheese
+bash saycheese.sh
+fi
+
+if [ $ok = 3 ] || [ $ok = 3 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt install python2
+apt install git
+git clone https://github.com/MrR4VEN/Deface
+mv Deface $HOME
+cd $HOME/Deface
+python2 deface.py
+fi
+
+if [ $ok = 4 ] || [ $ok = 4 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git && apt install python2
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+python2 sqlmap.py
+fi
+
+if [ $ok = 5 ] || [ $ok = 5 ] 
+then 
+clear 
+figlet "MrRaven" | lolcat 
+apt update && apt upgrade 
+apt install php 
+apt install curl 
+git clone https://github.com/Tuhinshubhra/RED_HAWK 
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+php rhawk.php 
+fi
+
+if [ $ok = 6 ] || [ $ok = 6 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/shawarkhanethicalhacker/D-TECT.git
+mv D-TECT $HOME
+cd $HOME/D-TECT
+python2 d-tect.py
+fi
+
+if [ $ok = 7 ] || [ $ok = 7 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install toilet
+apt install php
+apt install git
+git clone https://github.com/4L13199/LITESPAM
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $ok = 8 ] || [ $ok = 8 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt upgrade && apt update
+apt install python
+pkg install git
+git clone https://github.com/4shadoww/hakkuframework
+mv hakkuframework $HOME
+cd $HOME/hakkuframework
+python hakku
+fi
+
+if [ $ok = 9 ] || [ $ok = 9 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/blackvkng/viSQL.git
+mv viSQL $HOME
+cd $HOME/viSQL
+chmod 777 viSQL.py
+python2 viSQL.py -h
+fi
+
+if [ $ok = 10 ] || [ $ok = 10 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install python
+apt install git
+git clone https://github.com/cyweb/hammer
+mv hammer $HOME
+cd $HOME/hammer
+python hammer.py
+fi
+
+if [ $ok = 11 ] || [ $ok = 11 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install python2
+pip2 install requests
+apt install git
+git clone https://github.com/websploit/websploit.git
+mv websploit $HOME
+cd $HOME/websploit
+chmod +x *
+pip2 install scapy
+python2 websploit
+fi
+
+if [ $ok = 12 ] || [ $ok = 12 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+mv xerxes $HOME
+cd $HOME/xerxes
+clang xerxes.c -o xerxes
+fi
+
+if [ $ok = 13 ] || [ $ok = 13 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/grafov/hulk
+mv hulk $HOME
+cd $HOME/hulk
+python2 hulk.py
+sleep 1
+echo "Untuk Attack Ketik python2 hulk.py www.Web Target.com" | lolcat
+fi
+
+if [ $ok = 14 ] || [ $ok = 14 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git
+apt install tor
+apt install python2
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+python2 torshammer.py
+fi
+
+if [ $ok = 15 ] || [ $ok = 15 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $ok = 16 ] || [ $ok = 16 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt install root-repo
+apt install aircrack-ng
+apt install iw
+apt install wireless-tools
+apt install ethtool
+apt install tsu
+apt install git python2
+git clone https://github.com/derv82/wifite
+mv wifite $HOME
+cd $HOME/wifite
+chmod +x wifite.py
+./wifite.py
+fi
+
+if [ $ok = 18 ] || [ $ok = 18 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt install python
+apt install git
+git clone https://github.com/avramit/instahack
+mv instahack $HOME
+cd $HOME/instahack
+chmod +x hackinsta.py
+pip install -r pass.txt
+pip install -r password.txt
+pip install -r pasa.txt
+python hackinsta.py
+fi
+
+if [ $ok = 19 ] || [ $ok = 19 ]
+then
+clear
+figlet "MrRaven" | lolcat
+sleep 1
+git clone https://github.com/Xi4u7/A-Rat.git
+mv A-Rat $HOME
+cd $HOME/A-Rat
+python2 A-Rat.py
+fi
+
+if [ $ok = 20 ] || [ $ok = 20 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update
+apt upgrade
+apt install git
+apt install python
+git clone https://github.com/maldevel/IPGeoLocation
+mv IPGeoLocation $HOME
+cd $HOME/IPGeoLocation
+chmod 775 ipgeolocation.py
+pip install -r requirements.txt
+sleep 1
+echo "Untuk Menjalankan Ketik python ipgeolocation.py -t <IP_TARGET>" | lolcat
+fi
+
+if [ $ok = 21 ] || [ $ok = 21 ]
+then
+clear
+figlet "MrRaven" | lolcat
+fi
+
+if [ $ok = 22 ] || [ $ok = 22 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt install git -y
+apt install python2 -y
+git clone https://github.com/CiKu370/ko-dork
+mv ko-dork $HOME
+cd $HOME/ko-dork
+python2 dork.py
+fi
+
+if [ $ok = 23 ] || [ $ok = 23 ]
+then
+clear
+figlet "MrRaven" | lolcat
+cd /storage/emulated/0
+apt update && apt upgrade
+apt install wget
+apt install python2
+apt install libcurl
+apt install openssl curl
+pip2 install urllib3 chardet certifi idna requests
+mkdir webdav $HOME
+cd $HOME/webdav
+wget https://pastebin.com/raw/HnVyQPtR -O webdav.py
+python2 webdav.py
+fi
+
+if [ $ok = 24 ] || [ $ok = 24 ]
+then
+clear
+toilet -f mono12 -F gay "MrRaven" | lolcat
+apt update && apt upgrade
+apt install git
+apt install python2
+apt install shell
+git clone https://github.com/Manisso/fsociety.git
+mv fsociety $HOME
+cd $HOME/fsociety
+./install.sh
+python2 fsociety.py
+fi
+
+if [ $ok = 25 ] || [ $ok = 25 ]
+then
+clear
+figlet "MrRaven" | lolcat
+apt update && apt upgrade -y
+apt install root-repo
+apt install unstable-repo
+apt install x11-repo
+apt install curl
+apt install wget
+apt install git
+apt install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+fi
+
+if [ $ok = 0 ] || [ $ok = 00 ]
+then
+clear
+sleep 1
+echo "We are Security" | lolcat
+sleep 1
+echo "We not Friends" | lolcat
+sleep 1
+echo "We Are Family" | lolcat
+exit
+fi
